a1e99deccbb022c88c7ee409089953ee409d2789
Belanova2022 <belanova.kseniya@mail.ru>
2023-11-14 16:02:47 +0300

    Дженерики

---
+++ src/Gener.java
@@ -0,0 +1,43 @@
+public class Gener <T, V, K> {
+    private T ten;
+    private V ven;
+    private K ken;
+
+    public Gener(T ten, V ven, K ken) {
+        this.ten = ten;
+        this.ven = ven;
+        this.ken = ken;
+    }
+
+    public T getTen() {
+        return ten;
+    }
+
+    public void setTen(T ten) {
+        this.ten = ten;
+    }
+
+    public V getVen() {
+        return ven;
+    }
+
+    public void setVen(V ven) {
+        this.ven = ven;
+    }
+
+    public K getKen() {
+        return ken;
+    }
+
+    public void setKen(K ken) {
+        this.ken = ken;
+    }
+
+    @Override
+    public String toString() {
+        return "Дженерик" +
+                " первый = " + ten +
+                ", второй = " + ven +
+                ", третий = " + ken;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Gener<String, String, String> gener = new Gener<>("dfgh", "fdgdfg", "fdg");
+        System.out.println(gener);
+
+    }
+}
